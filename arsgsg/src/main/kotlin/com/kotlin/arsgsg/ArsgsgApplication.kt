1682d8ac39400d481cf1b1498a3bb4aa765b7d20
jhyun <arsgsg1@gmail.com>
2022-10-14 12:44:03 +0900

    project init

---
+++ arsgsg/src/main/kotlin/com/kotlin/arsgsg/ArsgsgApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin.arsgsg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArsgsgApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArsgsgApplication>(*args)
+}
---
+++ arsgsg/src/test/kotlin/com/kotlin/arsgsg/ArsgsgApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.arsgsg
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArsgsgApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
